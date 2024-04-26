c8afd14823e8e546c3c34f14e9137f95399829a9
Datthachban12345 <datthachban12345@gmail.com>
2024-04-26 09:36:42 +0700

    first commit

---
+++ src/main/java/java1/Config.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java1;
+
+/**
+ *
+ * @author Thanh
+ */
+public interface Config {
+    String DB_URL = "jdbc:mysql://localhost:3306/san_pham";
+    String USERNAME = "root";
+    String PASSWORD = "";
+    
+            
+}
